6a733fd9db36928bb719e2818cb053ad40525268
camicastro <camicastro.11.94@gmail.com>
2023-07-18 12:41:11 -0300

    Ejercicios M1

---
+++ 01-IntroToCS/demo.js
@@ -0,0 +1,25 @@
+console.log("Conjunción - AND: ");
+
+console.log("true AND true: ", true && true);
+console.log("true AND false: ", true && false);
+console.log("false AND true: ", false && true);
+console.log("false AND false: ", false && false);
+
+console.log("Disyunción - OR: ");
+
+console.log("true OR true: ", true || true);
+console.log("true OR false: ", true || false);
+console.log("false OR true: ", false || true);
+console.log("false OR false: ", false || false);
+
+console.log("Disyunción - XOR: ");
+
+console.log("true XOR true: ", true ^ true);
+console.log("true XOR false: ", true ^ false);
+console.log("false XOR true: ", false ^ true);
+console.log("false XOR false: ", false ^ false);
+
+console.log("Conjunción - NOT: ");
+
+console.log("NOT true: ", !true);
+console.log("NOT false: ", !false);
